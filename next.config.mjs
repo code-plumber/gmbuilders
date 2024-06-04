57dd8a28c2a5b0be64043292250cc3c121fb4c4d
= <=>
2024-06-05 00:06:32 +0100

    3

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-      // Optional: Change the output directory `out` -> `dist`
-    distDir: 'dist',
-    exportTrailingSlash: true,
 };
 
 export default nextConfig;
