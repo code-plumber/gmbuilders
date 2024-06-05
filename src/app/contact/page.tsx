2482ddcd081ba699df918bf5ed110f5b8312f2fe
= <=>
2024-06-05 18:31:14 +0100

    4

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from "react";
+import ContactForm from '../components/contactform'
 
 export default function contact() {
+
+
+
   return (
-    <div>contact</div>
-  )
+    <div className="mx-auto max-w-2xl px-4 my-10">
+      <h1>Contact Us</h1>
+      <ContactForm/>
+    </div>
+  );
 }
